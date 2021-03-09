042b61e3ea0246abec099ffcc00b033b2a8be3ec
Skander Hatira <shatira@genossh.genouest.org>
2021-03-09 09:01:19 +0100

    config

--- slurmScript.sh
+++ slurmScript.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#SBATCH --job-name=smkgddh
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=100
+#SBATCH --output=out_gddh.txt
 
-. /local/env/envsnakemake-5.5.4.sh
-snakemake --configfile config/config${SLURM_ARRAY_TASK_ID}.yaml --cores 40 --use-conda -k
+. /local/env/envsnakemake-5.20.1.sh
+snakemake --profile config/profiles/slurm --unlock
+time snakemake --profile config/profiles/slurm 
