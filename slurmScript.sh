32c55e85d8b9a4c287a7efdd2a69d8d569df525a
Skander Hatira <shatira@genossh.genouest.org>
2021-04-15 15:13:33 +0200

    fixing config file for fullAnalysis

--- slurmScript.sh
+++ slurmScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=smkgddh
+#SBATCH --job-name=smk
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=100
-#SBATCH --output=rerun.txt
+#SBATCH --output=out.txt
 
 . /local/env/envsnakemake-5.20.1.sh
 snakemake --profile config/profiles/slurm --unlock
-time snakemake --profile config/profiles/slurm 
+time snakemake --profile config/profiles/slurm
