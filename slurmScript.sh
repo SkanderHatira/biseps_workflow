44821331d391f1c71116c0de8fe77882ad88f85a
Skander Hatira <shatira@genossh.genouest.org>
2021-04-19 11:58:35 +0200

    keep lock to safeguard running jobs

--- slurmScript.sh
+++ slurmScript.sh
@@ -2,8 +2,7 @@
 #SBATCH --job-name=smkFull
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=100
-#SBATCH --output=out.txt
+#SBATCH --output=last.txt
 
 . /local/env/envsnakemake-5.20.1.sh
-snakemake --profile config/profiles/slurm --unlock
 time snakemake --profile config/profiles/slurm
