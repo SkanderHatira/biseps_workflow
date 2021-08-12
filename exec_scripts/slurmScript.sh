22d06ad27c3e77ce71ed3c7cd02117ef6a7a64c2
Skander Hatira <skander.hatira@inrae.fr>
2021-08-12 13:36:29 +0200

    adding utf8 encoding for remote slurm execution

--- exec_scripts/slurmScript.sh
+++ exec_scripts/slurmScript.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=1000
 #SBATCH --output=biseps.txt
-
+export LC_ALL=ca_FR.utf8
 source exec_scripts/script.sh 
 snakemake --profile config/profiles/slurm --unlock
 snakemake --profile config/profiles/slurm
