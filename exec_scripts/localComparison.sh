c3d0376bf558ac69c6ae9d54e816c1da96d3d81c
Skander Hatira <skander.hatira@inrae.fr>
2021-08-23 15:24:01 +0200

    making config files by default and trying out the execution without the unlock flag

--- exec_scripts/localComparison.sh
+++ exec_scripts/localComparison.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 
-snakemake --profile config/profiles/localComparison --unlock
+# snakemake --profile config/profiles/localComparison --unlock
 snakemake --profile config/profiles/localComparison 
--- exec_scripts/localScript.sh
+++ exec_scripts/localScript.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 
-snakemake --profile config/profiles/local --unlock
+# snakemake --profile config/profiles/local --unlock
 snakemake --profile config/profiles/local 
