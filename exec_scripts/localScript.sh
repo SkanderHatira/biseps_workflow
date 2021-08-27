49977d4eee6bbe69d0efc97f8fff28b807d2fa0d
Skander Hatira <skander.hatira@inrae.fr>
2021-08-27 14:05:57 +0200

    exec_scripts fix

--- exec_scripts/localScript.sh
+++ exec_scripts/localScript.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 unlock=$1
 if [[ $unlock ]]; then
-snakemake --profile config/profiles/test --unlock
+snakemake --profile config/profiles/test --unlock &>> biseps.txt 
 fi
 # snakemake --profile config/profiles/local --unlock
 source exec_scripts/script.sh 
-snakemake --profile config/profiles/test &> biseps.txt 
+snakemake --profile config/profiles/test &>> biseps.txt 
 snakemake --profile config/profiles/test --report report.html   &>> biseps.txt 
