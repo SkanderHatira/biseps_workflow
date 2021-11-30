764f8abd19f7f186967eff763cbf22332f16d332
Skander Hatira <skander.hatira@inrae.fr>
2021-11-30 16:08:05 +0100

    adding reading reports and methylation and coverage stats generation

--- workflow/scripts/methylkit.R
+++ workflow/scripts/methylkit.R
@@ -26,3 +26,43 @@ qValue <- 0.01 #snakemake@params[["qValue"]]
 
 ### inferred params  ###
 method <- if(bins) "bins" else "bases"
+
+### create output directory if it doesn't already exist
+dir.create(file.path(outdir), showWarnings = FALSE)
+
+### reading bismark's CX reports, by default using the tabix database for minimal memory usage
+readingReports=methRead(files,
+           sample.id=c(rep("control",length(control)),rep("treatment",length(treatment))),
+           assembly="bismark",
+           treatment=c(rep(0,length(control)),rep(1,length(treatment))),
+           context=context,
+           mincov = minCov,
+           pipeline="bismarkCytosineReport",
+           dbtype = "tabix",
+           dbdir = "methylDB"
+           )
+
+
+### meth stats and plot ###
+for (i in 1:length(files)) {
+	fileTxt<-file(snakemake@output[["methylationStatsTxt"]])
+    sink(fileTxt)
+    getMethylationStats(readingReports[[i]],plot=FALSE,both.strands=FALSE)
+    sink()
+    close(fileTxt)
+	fileTxt<-file(snakemake@output[["methylationStatsPdf"]])
+    getMethylationStats(readingReports[[i]],plot=TRUE,both.strands=FALSE)
+    dev.off()
+    }
+
+### coverage stats and plot ###
+for (i in 1:length(files)) {
+	fileTxt<-file(snakemake@output[["coverageStatsTxt"]])
+    sink(fileTxt)
+    getCoverageStats(readingReports[[i]],plot=FALSE,both.strands=FALSE)
+    sink()
+    close(fileTxt)
+	fileTxt<-file(snakemake@output[["coverageStatsPdf"]])
+    getCoverageStats(readingReports[[i]],plot=TRUE,both.strands=FALSE)
+    dev.off()
+    }
